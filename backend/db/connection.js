6d314e3778b8025b8440abdf30ecd771bd2aff14
Shagil1 <shagil@spydra.app>
2024-07-25 00:00:30 +0530

    init

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const connection = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("Connected to DB Server");
-    } catch (error) {
-        console.log('Error while connecting', error.message);
-    }
-}
+  try {
+    console.log(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to DB Server");
+  } catch (error) {
+    console.log("Error while connecting", error.message);
+  }
+};
 
-export default connection
+export default connection;
--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,41 @@
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import path from 'path'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path";
+import authRoutes from "./routes/auth.routes.js";
+import taskRoutes from "./routes/task.routes.js";
+import connection from "./db/connection.js";
+import cors from "cors";
 
+dotenv.config();
 
-import authRoutes from './routes/auth.routes.js'
-import taskRoutes from './routes/task.routes.js'
-import connection from "./db/connection.js"
-import cors from "cors"
+const app = express();
 
-const app = express()
+const PORT = process.env.PORT;
+console.log(PORT);
 
-dotenv.config()
+const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 5000
+app.use(express.json());
+app.use(cookieParser());
 
-const __dirname = path.resolve()
-
-
-app.use(express.json())
-app.use(cookieParser())
-
-app.use(cors({
-    origin: ['http://localhost:3000'],
+app.use(
+  cors({
+    origin: [process.env.URI],
     credentials: true,
-}));
+  }),
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/task", taskRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/task", taskRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-})
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-    connection()
-})
+  console.log(`Server is running on ${PORT}`);
+  connection();
+});
